527a4cc9445d2d699558bd848ac3291abd9f7659
Ratan Sharma <140261552+rat145@users.noreply.github.com>
2024-05-17 12:08:22 +0000

    datatypes summary done

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,25 @@
+// Primitive
+
+// 7 Types: String, number, Boolean, null, undefned, symbol, bigint
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);
+console.log(anotherId);
+console.log(id == anotherId);
+
+const bigNumber = 1234567876542312456n //big int
+
+
+// Reference (non-primitive)
+
+// array, objects, functions
+
+const movies = ["the shawshank redemption", "little sunshine", "chemical hearts"]
+const myObj = {
+    name: "ratan",
+    age: 20,
+}
+const myFunction = function() {
+    console.log("Hello world")
+}
