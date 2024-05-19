8ce0949c1f8858485bd393718ef1de7e78bb63b8
Ratan Sharma <140261552+rat145@users.noreply.github.com>
2024-05-19 08:22:01 +0000

    created

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,27 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("hitesh-hc-com")
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3))
+// console.log(gameName.indexOf('t'))
+
+// console.log(gameName.substring(0, 4));
+// console.log(gameName.slice(-8, 4));
+
+const newString = "     hitesh     "
+console.log(newString.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace("%20", "-"));
+console.log(url.includes("rat"));
+console.log(gameName.split("-"));
