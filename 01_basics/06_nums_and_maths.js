cbd8e7fde2f67b2a18ade5a94f624e8813633e3b
Ratan Sharma <140261552+rat145@users.noreply.github.com>
2024-05-20 06:48:56 +0000

    updated

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,5 +1,26 @@
 const score = 400
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.50;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"))
+
+// ++++++++++++ MATHS ++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,3,2,8))
+// console.log(Math.max(3,4,6,1))
+
